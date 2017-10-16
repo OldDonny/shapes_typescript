c39512e664211316544239e6615d1d74ba7a6b98
reed <reedtrekks@gmail.com>
2017-10-16 08:59:51 -0500

    sass and buttons

---
+++ script.ts
@@ -0,0 +1,11 @@
+
+let rectangle_btn= document.getElementById("rectangle_btn");
+let  square_btn= document.getElementById("square_btn");
+let  circle_btn= document.getElementById("circle_btn");
+let  triangle_btn= document.getElementById("triangle_btn");
+
+rectangle_btn.addEventListener
+
+class shape{
+
+}
